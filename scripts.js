c8e22bd34ee3f26340c1a2223c65318c61ab93f1
Alex Baker <37917867+alxbaker@users.noreply.github.com>
2023-03-05 13:07:21 -0600

    Create operate function

--- scripts.js
+++ scripts.js
@@ -2,20 +2,32 @@ const display = document.querySelector('#display');
 const allBtns = document.querySelectorAll("button");
 
 const add = function(a, b) {
-	return a + b;
+	return +a + +b;
 };
 
 const subtract = function(a, b) {
-	return a - b;
+	return +a - +b;
 };
 
 const multiply = function(a, b) {
-	return a * b;
+	return +a * +b;
 };
 
 const divide = function(a, b) {
-	return a / b;
+	return +a / +b;
 };
+
+const operate = function(a, b, operator) {
+    if (operator == '+') {
+        return add(a, b);
+    } else if (operator == '-') {
+        return subtract(a, b);
+    } else if (operator == '/') {
+        return divide(a, b);
+    } else if (operator == '*') {
+        return multiply(a, b);
+    }
+}
 
 allBtns.forEach((button) => {
     const keyContent = button.textContent;
