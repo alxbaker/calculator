8e2952a77b891e6cf852d179479cdbda9b085077
Alex Baker <37917867+alxbaker@users.noreply.github.com>
2023-03-05 12:52:17 -0600

    Create add, subtract, multiply, divide functions

--- scripts.js
+++ scripts.js
@@ -1,6 +1,22 @@
 const display = document.querySelector('#display');
 const allBtns = document.querySelectorAll("button");
 
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const multiply = function(a, b) {
+	return a * b;
+};
+
+const divide = function(a, b) {
+	return a / b;
+};
+
 allBtns.forEach((button) => {
     const keyContent = button.textContent;
     button.addEventListener('click', () => {
